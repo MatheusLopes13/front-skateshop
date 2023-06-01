057f486d4e19abb1391e989b340b0b33e49c31ac
gooncalvesgaby <gabrielle.gonalves@outlook.com.br>
2023-06-01 01:30:31 -0300

    tela de login

---
+++ src/pages/Login/login.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "../includes/Header/index.jsx"
+import Footer from "../includes/Footer/index.jsx"
+const Login = () => {
+return (
+    <>
+        <Header/>
+            <p>Login</p>
+            <p>Login</p>
+        <Footer/>
+
+    </>
+ )
+}
+
+export default Login
--- src/router.jsx
+++ src/router.jsx
@@ -5,12 +5,15 @@ import {
     Route
 } from 'react-router-dom'
 import Home from "./pages/Home";
+import Login from "./pages/Login/login";
+
 
 const Router = () => {
 return (
     <BrowserRouter>
         <Routes>
           <Route exact path="/" element={ <Home /> } />
+          <Route exact path="/login" element={<Login />} />
         </Routes>
     </BrowserRouter>
 )
